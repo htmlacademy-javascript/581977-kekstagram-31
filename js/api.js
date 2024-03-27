b8e5f03f977aa3ac0cf64d69476073e9edbfa519
Ilia Cheshev <ilyacheshev@gmail.com>
2024-03-27 22:50:03 +0300

    add api and handlers for success and error cases

---
+++ js/api.js
@@ -0,0 +1,20 @@
+import {cloneTemplate} from './utils.js';
+
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+
+const errorMessageHandler = () => {
+  cloneTemplate('data-error');
+  setTimeout(() => {
+    document.querySelector('.data-error').remove();
+  }, 5000);
+};
+
+const getData = () => fetch(`${BASE_URL}/data`)
+  .then((response) => response.json())
+  .catch(errorMessageHandler);
+
+const sendData = (body, onSuccess, onError) => fetch(`${BASE_URL}`, {method: 'POST', body: body})
+  .then((response) => response.ok ? onSuccess() : onError())
+  .catch(() => onError());
+
+export {getData, sendData};
--- js/form.js
+++ js/form.js
@@ -1,15 +1,22 @@
 import './effects.js';
 import {addScaleListeners, removeScaleListeners} from './scale.js';
 import {validateForm} from './validators.js';
+import {sendData} from './api.js';
+import {cloneTemplate} from './utils.js';
 
+const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
 const imgUploadPreviewImg = document.querySelector('.img-upload__preview img');
 
 const resetSettings = (callback) => {
   document.querySelector('.img-upload__overlay').classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
+  body.classList.remove('modal-open');
   document.removeEventListener('keydown', callback);
+  document.removeEventListener('click', callback);
   removeScaleListeners();
+  if (body.contains(document.querySelector('.success'))) {
+    document.querySelector('.success').remove();
+  }
   form.reset();
 };
 
@@ -22,13 +29,53 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-const onCloseButtonClick = () => {
-  resetSettings(onCloseButtonClick);
+const onDocumentKeydownModal = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    document.querySelector('.error').remove();
+    document.addEventListener('keydown', onDocumentKeydown);
+    document.removeEventListener('keydown', onDocumentKeydownModal);
+  }
+};
+
+const onOutsideClick = (evt) => {
+  if (evt.target === document.querySelector('.success')) {
+    document.querySelector('.success').remove();
+    resetSettings(onOutsideClick);
+  } else if (evt.target === document.querySelector('.error')) {
+    document.querySelector('.error').remove();
+    document.removeEventListener('click', onOutsideClick);
+  }
+};
+
+const onCloseButtonClick = (evt) => {
+  if (evt.target === document.querySelector('.success__button')) {
+    document.querySelector('.success').remove();
+    resetSettings(onCloseButtonClick);
+  } else if (evt.target === document.querySelector('.error__button')) {
+    document.querySelector('.error').remove();
+  } else {
+    resetSettings(onCloseButtonClick);
+  }
+};
+
+const onSuccess = () => {
+  cloneTemplate('success');
+  document.addEventListener('click', onOutsideClick);
+  document.querySelector('.success__button').addEventListener('click', onCloseButtonClick);
+};
+
+const onError = () => {
+  document.removeEventListener('keydown', onDocumentKeydown);
+  cloneTemplate('error');
+  document.addEventListener('keydown', onDocumentKeydownModal);
+  document.addEventListener('click', onOutsideClick);
+  document.querySelector('.error__button').addEventListener('click', onCloseButtonClick);
 };
 
 document.querySelector('.img-upload__input').addEventListener('change', (evt) => {
   document.querySelector('.img-upload__overlay').classList.remove('hidden');
-  document.querySelector('body').classList.add('modal-open');
+  body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
   document.querySelector('.img-upload__cancel').addEventListener('click', onCloseButtonClick);
   addScaleListeners();
@@ -46,5 +93,14 @@ document.querySelector('.img-upload__input').addEventListener('change', (evt) =>
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  validateForm();
+  const isValid = validateForm();
+  const requestBody = new FormData(evt.target);
+  if (isValid) {
+    const submitButton = form.querySelector('.img-upload__submit');
+    submitButton.disabled = true;
+    sendData(requestBody, onSuccess, onError)
+      .finally(() => {
+        submitButton.disabled = false;
+      });
+  }
 });
